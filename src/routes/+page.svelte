de9fa3365fa5ee02c354f69762759abcb0c7d152
awal-cendana <awal-cendana1@outlook.com>
2024-08-06 13:42:11 +0000

    5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,24 @@
 <script>
+	import axios from 'axios'
+	import { onMount } from 'svelte';
+	let dataManual = []
+	let apiUrl = import.meta.env.VITE_API_URL
+
     let tanggal = ''
     // simpan tanggal di localStorage
     const saveTanggal = () => {
         localStorage.setItem('tanggal', tanggal)
         console.log(localStorage.getItem('tanggal'))
     }
+	const getData = async () => {
+		await axios.get(`${apiUrl}.json`).then((response) => {
+			const data = Object.values(response.data)
+			dataManual = data
+		})
+	}
+	onMount(() => {
+		getData()
+	})
 </script>
 
 <div class="container">
@@ -17,6 +31,40 @@
 			<div class="d-flex form-group">
 				<input type="text" class="form-control" placeholder="Tanggal Datang Tabung" bind:value={tanggal} />
                 <button class="btn btn-primary" on:click={saveTanggal}>Simpan</button>
+			</div>
+		</div>
+		<div class="col-md-10">
+			<h1>Catat Manual Transaksi LPG</h1>
+			<div>
+				<a href="/create" class="btn btn-primary">Create</a>
+				<!-- table data -->
+				<table class="table">
+					<thead>
+						<tr>
+							<th scope="col">NIK</th>
+							<th scope="col">Nama</th>
+							<th scope="col">Kategori</th>
+							<th scope="col">Jumlah Tabung</th>
+							<th scope="col">Tanggal</th>
+
+							<th scope="col">Action</th>
+						</tr>
+					</thead>
+					<tbody>
+						{#each dataManual as item}
+						<tr>
+							<td>{item.nik}</td>
+							<td>{item.nama}</td>
+							<td>{item.kategori}</td>
+							<td>{item.jtabung}</td>
+							<td>{item.tanggal}</td>
+
+							<td></td>
+						</tr>
+						{/each}
+					</tbody>
+				</table>
+
 			</div>
 		</div>
 	</div>
