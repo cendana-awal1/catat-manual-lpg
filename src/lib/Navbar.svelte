d1af290cc618a1bffa96124fa56712e4ac40e136
awal-cendana <awal-cendana1@outlook.com>
2024-08-06 14:27:43 +0000

    10

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,11 +1,21 @@
 <script>
 </script>
+
 <nav class="navbar bg-dark navbar-dark fixed-top">
 	<div class="container-fluid">
-	  <a href="/" class="navbar-brand">Pangkalan LPG</a>
-	  <form class="d-flex" role="search">
-		<input class="form-control me-2" type="text" placeholder="Input Tanggal" aria-label="text">
-		<button class="btn btn-outline-success" type="submit">Simpan</button>
-	  </form>
+		<a href="/" class="navbar-brand">Pangkalan LPG</a>
+		<!-- home -->
+		<ul class="navbar-nav">
+			<li class="nav-item">
+				<a class="nav-link active" aria-current="page" href="/">Home</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="/create">Create</a>
+			</li>
+		</ul>
+		<form class="d-flex" role="search">
+			<input class="form-control me-2" type="text" placeholder="Input Tanggal" aria-label="text" />
+			<button class="btn btn-outline-success" type="submit">Simpan</button>
+		</form>
 	</div>
-  </nav>
+</nav>
