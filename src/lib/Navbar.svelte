c7a9abebc778044ad3fe2cc8cba5edb8006b4f54
awal-cendana <awal-cendana1@outlook.com>
2024-08-06 14:33:15 +0000

    11

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -5,14 +5,13 @@
 	<div class="container-fluid">
 		<a href="/" class="navbar-brand">Pangkalan LPG</a>
 		<!-- home -->
-		<ul class="navbar-nav">
-			<li class="nav-item">
-				<a class="nav-link active" aria-current="page" href="/">Home</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="/create">Create</a>
-			</li>
-		</ul>
+
+		<span class="d-flex">
+			<a class="nav-link active" aria-current="page" href="/">Home</a>
+
+			<a class="nav-link" href="/create">Create</a>
+		</span>
+
 		<form class="d-flex" role="search">
 			<input class="form-control me-2" type="text" placeholder="Input Tanggal" aria-label="text" />
 			<button class="btn btn-outline-success" type="submit">Simpan</button>
